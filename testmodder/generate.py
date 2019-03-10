0fba8d79ae98b6f23a911bca1bacbd970896794a
Neil Patil <neilpatil215@gmail.com>
2019-03-10 13:43:51 -0500

    Add python script to modify test to create macro

---
+++ testmodder/generate.py
@@ -0,0 +1,58 @@
+import click
+import os
+
+def file_to_lines(filename):
+    return open(filename).read().splitlines()
+
+@click.command()
+@click.argument('filename')
+def transform(filename):
+    lines = open(filename).read().splitlines()
+
+    # Find the leading comments, and add the '--allow-natives-syntax' flag
+    comments_or_empty_ind = 0
+    foundFlags = False
+    while comments_or_empty_ind < len(lines):
+        line = lines[comments_or_empty_ind]
+
+        if not (line.startswith('//') or line.strip() == ''):
+            break
+
+        if line.startswith('// Flags:') and 'allow-natives-syntax' not in line:
+            line += ' --allow-natives-syntax'
+            lines[comments_or_empty_ind] = line
+            foundFlags = True
+
+        comments_or_empty_ind += 1
+
+    # Back up the leading comments, because they get stripped by sweet.js
+    lines_to_save = lines[:comments_or_empty_ind]
+    if not foundFlags:
+        lines_to_save.append(open('allow_natives_flag.js').read().splitlines()[0])
+
+
+    lines_to_modify = lines[comments_or_empty_ind:]
+
+    # Prepend our modifications
+    modifications = []
+    modifications += file_to_lines("helpers.sweet")
+    modifications += file_to_lines("assert_equals.sweet")
+
+    lines_to_modify = modifications + lines_to_modify
+
+    # Write lines to sweet to file
+    with open(filename + '.sweet', 'w') as sweetfile:
+        sweetfile.writelines('\n'.join(lines_to_modify) + '\n')
+
+    # Run sweet on the file
+    os.system("sjs --out-file " + filename + '.compiled ' + filename + '.sweet')
+
+    # Concatenate this file with the lines we saved
+    lines = lines_to_save + file_to_lines(filename + '.compiled')
+
+    # Output our final modified file
+    with open(filename, 'w') as outfile:
+        outfile.writelines('\n'.join(lines) + '\n')
+
+if __name__ == '__main__':
+    transform()
