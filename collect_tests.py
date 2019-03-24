851b1c9575d8ac32d1b5996f9884d3ab2f52951b
patil215 <neilpatil215@gmail.com>
2019-03-24 17:23:54 -0500

    Add script to generate list of tests

---
+++ collect_tests.py
@@ -0,0 +1,21 @@
+import os
+import click
+
+@click.command()
+@click.option("--source-file", default="sources.txt")
+@click.option("--out-file", default="typer-happy-tests.txt")
+@click.option("--tests-dir", default="test/mjsunit")
+def generate(source_file, out_file, tests_dir):
+    sources = open(source_file).readlines()
+    open(out_file, 'w').close()
+
+    for line in sources:
+        source = line[:line.find('\"')]
+        if len(source) > 2:
+            print(source)
+            command = "grep -r -l {} {} >> {}".format(source, tests_dir, out_file)
+            print(command)
+            os.system(command)
+
+if __name__ == '__main__':
+    generate()
