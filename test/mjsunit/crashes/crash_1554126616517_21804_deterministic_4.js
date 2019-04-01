05358c80efa93a6710eb175c4a7e988864008521
Neil Patil <neilpatil215@gmail.com>
2019-04-01 10:34:53 -0500

    Put crashes in testable place

---
+++ test/mjsunit/crashes/crash_1554126616517_21804_deterministic_4.js
@@ -0,0 +1,17 @@
+// Flags: --allow-natives-syntax
+
+function main() {
+for (let v3 = 0; v3 < 100; v3++) {
+    const v6 = [1337,1337,1337,1337];
+    const v7 = [v6,1,1337];
+    function v8(v9,v10,v11,v12) {
+        const v14 = [13.37];
+        let v15 = v14;
+        const v16 = v15.indexOf();
+        const v17 = v16 >> v7;
+    }
+    const v18 = v8();
+}
+}
+%NeverOptimizeFunction(main);
+main();
---
+++ test/mjsunit/crashes/crash_1554130764774_20646_deterministic_4.js
@@ -0,0 +1,41 @@
+// Flags: --allow-natives-syntax
+
+function main() {
+const v2 = [1337];
+let v3 = Math;
+for (let v7 = 0; v7 < 5; v7++) {
+    const v9 = Function + 1;
+    function v10(v11,v12,v13,v14,v15) {
+        let v19 = "undefined";
+        let v23 = 0;
+        while (v23 < 3) {
+            v23 = 10;
+        }
+        const v24 = v9.codePointAt(v14,v3,v15,v2);
+        for (const v25 in v19) {
+        }
+        let v27 = "undefined";
+        let v31 = 0;
+        while (v31 < 3) {
+            v31 = 10;
+        }
+        for (const v32 in v27) {
+        }
+        for (const v34 in Function) {
+        }
+        for (let v36 = 0; v36 < 100; v36++) {
+        }
+        for (let v40 = 0; v40 < 5; v40++) {
+            const v42 = {};
+            const v43 = Math != v42;
+            let v44 = v43;
+            for (const v45 in v44) {
+            }
+        }
+        return v24;
+    }
+    const v46 = v10();
+}
+}
+%NeverOptimizeFunction(main);
+main();
